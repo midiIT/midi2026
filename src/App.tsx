618ecbf1713c4438c653e9adeb3f6e6bc565c2ca
Justinas P <justinas.pranaitis@mif.stud.vu.lt>
2026-01-07 23:55:53 +0200

    Added first room

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import RoomGrid from './components/RoomGrid';
+import kingRoomDesktop from './assets/rooms/kingRoom1.png';
+import kingRoomMobile from './assets/rooms/kingRoom2.png';
+import useResponsiveTiles from './hooks/useResponsiveTiles';
 
 function App() {
 
+  const { isMobile } = useResponsiveTiles();
+  const kingRoomBg = isMobile ? kingRoomMobile : kingRoomDesktop;
+
   const rooms = [
-    { id: 1, content: <span>Kambarys 1</span> },
+    { id: 1, content: <img src={kingRoomBg} alt="Sosto kambarys" className="w-full h-full" />, background: kingRoomBg },
     { id: 2, content: <span>Kambarys 2</span> },
     { id: 3, content: <span>Kambarys 3</span> },
     { id: 4, content: <span>Kambarys 4</span> },
