b85e918a0fb50ba0e9f4ad4b8ee4428628669bc8
Kipras Gnižinskas <kiprijonas123@gmail.com>
2026-01-27 00:05:31 +0200

    Fix centering in desktop view

--- src/pages/rooms/SocialMediaMIDI.tsx
+++ src/pages/rooms/SocialMediaMIDI.tsx
@@ -16,9 +16,9 @@ export default function SocialMediaMIDI() {
             background={bck}
             className="text-black text-center"
         >
-            <h2 className="text-2xl font-bold mb-[1.6rem] sm:mb-[3.2rem]">MIDI socialinių tinklų kambarys</h2>
+            <h2 className="text-2xl font-bold mb-[1.6rem]">MIDI socialinių tinklų kambarys</h2>
 
-            <div className="grid gap-2 grid-cols-1 sm:grid-cols-3 items-center justify-items-center mt-4">
+            <div className="grid gap-2 grid-cols-1 sm:grid-cols-3 items-center h-[80%]">
                 <a href="https://www.facebook.com/midi.lt/" target="_blank" className="inline-block" rel="noopener noreferrer">
                     <div className="flex items-center gap-3 p-[1.6rem] flex-col">
                         <img src={FacebookIcon} className="w-[5rem] sm:w-[8rem] h-auto object-contain" alt="Facebook" />
