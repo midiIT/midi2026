46ea2a8f99a8c57d1813b8e5f781367a75881b75
Kipras Gnižinskas <kiprijonas123@gmail.com>
2026-01-26 23:46:09 +0200

    Remove info paragraph and add slight padding

--- src/pages/rooms/SocialMediaMIDI.tsx
+++ src/pages/rooms/SocialMediaMIDI.tsx
@@ -16,24 +16,23 @@ export default function SocialMediaMIDI() {
             background={bck}
             className="text-black text-center"
         >
-            <h2 className="text-2xl font-bold mb-4">MIDI socialinių tinklų kambarys</h2>
-            <p className="mb-4">Daugiau informacijos apie MIDI galite rasti populiariausiose socialinių tinklų platformose.</p>
+            <h2 className="text-2xl font-bold mb-[1.6rem] sm:mb-[3.2rem]">MIDI socialinių tinklų kambarys</h2>
 
             <div className="grid gap-2 grid-cols-1 sm:grid-cols-3 items-center justify-items-center mt-4">
                 <a href="https://www.facebook.com/midi.lt/" target="_blank" className="inline-block" rel="noopener noreferrer">
-                    <div className="flex items-center gap-3 flex-col">
+                    <div className="flex items-center gap-3 p-[1.6rem] flex-col">
                         <img src={FacebookIcon} className="w-[5rem] sm:w-[8rem] h-auto object-contain" alt="Facebook" />
                         <span className="text-sm sm:text-base font-medium">Facebook</span>
                     </div>
                 </a>
                 <a href="https://www.instagram.com/midi.lt/" target="_blank" className="inline-block relative" rel="noopener noreferrer">
-                    <div className="flex items-center gap-3 flex-col">
+                    <div className="flex items-center gap-3 p-[1.6rem] flex-col">
                         <img src={InstagramIcon} className="w-[5rem] sm:w-[8rem] h-auto object-contain" alt="Instagram" />
                         <span className="text-sm sm:text-base font-medium">Instagram</span>
                     </div>
                 </a>
                 <a href="https://www.tiktok.com/@midi.lt" target="_blank" className="inline-block" rel="noopener noreferrer">
-                    <div className="flex items-center gap-3 flex-col">
+                    <div className="flex items-center gap-3 p-[1.6rem] flex-col">
                         <img src={TikTokIcon} className="w-[5rem] sm:w-[8rem] h-auto object-contain" alt="TikTok" />
                         <span className="text-sm sm:text-base font-medium">TikTok</span>
                     </div>
