2ab469ed0df0598aed37d6f5ece346f3ece8dbc0
Kipras Gnižinskas <kiprijonas123@gmail.com>
2026-01-27 00:27:18 +0200

    Fix alignment and remove info paragraph

--- src/pages/rooms/SocialMediaRO.tsx
+++ src/pages/rooms/SocialMediaRO.tsx
@@ -16,24 +16,23 @@ export default function SocialMediaRO() {
             background={bck}
             className="text-black text-center"
         >
-            <h2 className="text-2xl font-bold mb-4">RO socialinių tinklų kambarys</h2>
-            <p className="mb-4">Daugiau informacijos apie RO galite rasti populiariausiose socialinių tinklų platformose.</p>
+            <h2 className="text-2xl font-bold mb-[1.6rem]">RO socialinių tinklų kambarys</h2>
 
-            <div className="grid gap-2 grid-cols-1 sm:grid-cols-3 items-center justify-items-center mt-4">
+            <div className="grid gap-2 grid-cols-1 sm:grid-cols-3 items-center h-[80%]">
                 <a href="https://www.facebook.com/rokooperos/" target="_blank" className="inline-block" rel="noopener noreferrer">
-                    <div className="flex items-center gap-3 flex-col">
+                    <div className="flex items-center gap-3 p-[1.6rem] flex-col">
                         <img src={FacebookIcon} className="w-[5rem] sm:w-[8rem] h-auto object-contain" alt="Facebook" />
                         <span className="text-sm sm:text-base font-medium">Facebook</span>
                     </div>
                 </a>
                 <a href="https://www.instagram.com/roko_operos/" target="_blank" className="inline-block relative" rel="noopener noreferrer">
-                    <div className="flex items-center gap-3 flex-col">
+                    <div className="flex items-center gap-3 p-[1.6rem] flex-col">
                         <img src={InstagramIcon} className="w-[5rem] sm:w-[8rem] h-auto object-contain" alt="Instagram" />
                         <span className="text-sm sm:text-base font-medium">Instagram</span>
                     </div>
                 </a>
                 <a href="https://www.tiktok.com/@midi.roko.operos" target="_blank" className="inline-block" rel="noopener noreferrer">
-                    <div className="flex items-center gap-3 flex-col">
+                    <div className="flex items-center gap-3 p-[1.6rem] flex-col">
                         <img src={TikTokIcon} className="w-[5rem] sm:w-[8rem] h-auto object-contain" alt="TikTok" />
                         <span className="text-sm sm:text-base font-medium">TikTok</span>
                     </div>
