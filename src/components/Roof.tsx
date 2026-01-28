f6d06572cf61c9a6d1b1de35e746af062c784a3f
Justinas P <justinas.pranaitis@mif.stud.vu.lt>
2026-01-28 17:39:18 +0200

    Changed Curtains and Cat to work with the new system

--- src/components/Roof.tsx
+++ src/components/Roof.tsx
@@ -4,16 +4,15 @@ interface RoofProps {
   roofWidth: number;
   roofHeight: number;
   columns: number;
-  tileSize: number;
 }
 
-export default function Roof({ roofWidth, roofHeight, columns, tileSize }: RoofProps) {
+export default function Roof({ roofWidth, roofHeight, columns }: RoofProps) {
   return (
     <div
       style={{
         display: 'flex',
         flexDirection: 'row',
-        marginBottom: -tileSize,
+        marginBottom: -24,
         zIndex: 20,
       }}
     >
@@ -21,9 +20,9 @@ export default function Roof({ roofWidth, roofHeight, columns, tileSize }: RoofP
         <div
           key={index}
           style={{
-            width: roofWidth,
+            width: roofWidth * 1.05,
             height: roofHeight,
-            marginLeft: index > 0 ? -tileSize : 0,
+            marginLeft: index === 0 ? 0 : -20,
           }}
         >
           <img 
@@ -33,7 +32,6 @@ export default function Roof({ roofWidth, roofHeight, columns, tileSize }: RoofP
               width: '100%',
               height: '100%',
               objectFit: 'fill',
-              paddingBottom: tileSize / 2,
             }}
           />
         </div>
