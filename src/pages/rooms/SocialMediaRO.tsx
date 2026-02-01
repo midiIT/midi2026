8c1bffbfba258be18a27d7b93ec2a812040a19f4
Kipras Gnižinskas <kiprijonas123@gmail.com>
2026-02-01 15:04:41 +0200

    Replace 'useResponsiveTiles' with 'useResponsiveLayout'

--- src/pages/rooms/SocialMediaRO.tsx
+++ src/pages/rooms/SocialMediaRO.tsx
@@ -1,4 +1,4 @@
-import useResponsiveTiles from "../../hooks/useResponsiveTiles";
+import useResponsiveLayout from "../../hooks/useResponsiveLayout";
 import RoomContent from "../RoomContent";
 import RoomPC from '../../assets/rooms/roSocialsRoomPC.png'
 import RoomMobile from '../../assets/rooms/roSocialsRoomMobile.png'
@@ -8,8 +8,8 @@ import TikTokIcon from '../../assets/social_icons/tiktok.png'
 
 export default function SocialMediaRO() {
   
-    const { isMobile } = useResponsiveTiles();
-    const bck = isMobile ? RoomMobile : RoomPC;
+    const { deviceType } = useResponsiveLayout();
+    const bck = deviceType === 'mobile' ? RoomMobile : RoomPC;
 
     return <div className="w-full h-full bg-blue-200 flex items-center justify-center">
         <RoomContent
